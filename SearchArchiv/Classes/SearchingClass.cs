4ffbf0497100d0c5afbc68f9dce4898ad595ae9b
nass1988 <60288472+nass1988@users.noreply.github.com>
2021-11-06 17:44:41 +0100

    Creating new Class form Searchings methode structur.

---
+++ SearchArchiv/Classes/SearchingClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchArchiv.Classes
+{
+    /// <summary>
+    /// Searching Class
+    /// </summary>
+    class SearchingClass
+    {
+        public void StartSearch(string IdNumber)
+        {
+
+        }
+    }
+}
